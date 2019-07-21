80ea53066488769a6207f51a8218b88a3408c46c
Melissa <melissaashleym@gmail.com>
2019-07-21 12:56:14 -0600

    api key should not be inside repo

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ import 'whatwg-fetch';
 
 let config = {
   apiRoot: 'https://api.harvardartmuseums.org/object?q=totalpageviews:0&size=10&apikey=',
-  apiKey: "5c335e70-a3d6-11e9-94bb-f30da93ec338",
+  apiKey: "YOURKEY",
 };
 
 export default config;
